248ddcd610335e3580def6a5c5026989cac7148f
Manish P <ManishPatil7058@gmail.com>
2023-02-09 16:22:13 +0530

    macros

--- v0.03/index.js
+++ v0.03/index.js
@@ -46,7 +46,7 @@ class pcBootC{
         this.powerB.style.marginLeft="auto";
         this.powerB.style.display="block";    
         this.powerB.style.marginTop="3vh";
-        this.powerB.onclick=this.powerBc;
+        this.powerB.onclick=()=>{this.powerBc(this);};
         this.resetB=document.createElement("button");
         this.resetB.innerHTML="R";
         this.resetB.style.width="20vmin";
@@ -60,7 +60,7 @@ class pcBootC{
         this.resetB.style.marginLeft="auto";
         this.resetB.style.display="block";
         this.resetB.style.marginTop="3vh";
-        this.resetB.onclick=this.resetB;
+        this.resetB.onclick=()=>{this.resetBc(this);};
         this.stat=document.createElement("div");
         this.stat.classList="genericText";
         this.stat.innerHTML="_status_";
@@ -74,7 +74,7 @@ class pcBootC{
         this.sel2=document.createElement("option");
         this.sel1.innerHTML="Windows";
         this.sel2.innerHTML="Linux";
-        this.sel.value="1";
+        this.sel1.value="1";
         this.sel2.value="2";
         this.sel.style.fontSize="10vmin";
         this.sel.style.width="auto";
@@ -93,15 +93,37 @@ class pcBootC{
         content.appendChild(this.stat);
         content.appendChild(this.resetB);
         content.appendChild(this.selD);
-        this.updStat();
+        this.updStat(this);
     }
-    updStat(){}
-    powerBc(){}
-    resetBc(){}
+    updStat(me){
+        var http= new XMLHttpRequest();
+        http.onload=(e)=>{
+            me.stat.innerHTML=e.responseText;
+        }
+        http.open("GET",window.location.href+'/pStat');
+        http.send();
+    }
+    powerBc(me){
+        var http= new XMLHttpRequest();
+        http.onload=(e)=>{
+            me.stat.innerHTML=e.responseText;
+        }
+        http.open("GET",window.location.href+`/pcBoot/?b=p&os=${me.sel.value}`);
+        http.send();
+    }
+    resetBc(me){
+        var http= new XMLHttpRequest();
+        http.onload=(e)=>{
+            me.stat.innerHTML=e.responseText;
+        }
+        http.open("GET",window.location.href+'/pcBoot/?b=r');
+        http.send();
+    }
 }
 class keyBC{
     constructor(){
         content.innerHTML="";
+        menuBpress(0);
         this.key=[];
         this.keySpecial=[];
         this.keyComand=['ctrl','cmd','alt','&rarr;','&larr;','&uarr;','&darr;'];
@@ -113,11 +135,9 @@ class keyBC{
         }
         this.keyboard=document.createElement("div");
         this.keyboard.style.padding="10px";
-        this.keyboard.style.height="100%";
         this.keyboard.style.width="100%";
         this.keyboard.onkeydown= (e)=>{this.keyDownPhysic(this,e)};
         this.keyboard.onkeyup= (e)=>{this.keyUpPhysic(this,e)};
-        menuBpress(0);
         for (let i = 0; i < 33; i++) {
             this.key.push(document.createElement("button"));
             this.key[i].innerHTML=this.keySpecial[i];
@@ -173,6 +193,10 @@ class keyBC{
             this.key[i].style.border="0.5vmin solid rgba(0,100,100,.2)";
             this.key[i].style.borderRadius="1vmin";
             this.key[i].style.fontSize="2vw";
+            this.key[i].onmousedown=()=>{this.keyDownEvent(this.key[i],3);}
+            this.key[i].onmouseup=()=>{this.keyUpEvent(this.key[i],3);}
+            this.key[i].ontouchstart=()=>{this.keyDownEvent(this.key[i],3);}
+            this.key[i].ontouchend=()=>{this.keyUpEvent(this.key[i],3);}
             this.keyboard.appendChild(this.key[i]);
         }
         for (let i = 127; i < 132; i++) {
@@ -191,7 +215,7 @@ class keyBC{
         this.keyCaps();
     }
     keyUpEvent(key,mode){
-        console.log('u');
+        alert('u');
     }
     keyDownEvent(key,mode){
         console.log('d');
@@ -237,10 +261,9 @@ class keyBC{
     keyHandler(event) {
       }
 }
-
 class mouseBC{
     constructor(){
-        // menuBpress(0);
+        menuBpress(0);
         content.innerHTML="";
         this.mouseArea=document.createElement("div");
         this.rclk=document.createElement("button");
@@ -258,6 +281,7 @@ class mouseBC{
         this.mouseArea.onwheel=(e)=>{this.scrollCh(e,1);};
         this.mouseArea.ontouchend=()=>{this.mouseArea.style.border="";};
         this.mouseArea.onmouseleave=()=>{this.mouseArea.style.border="";};
+        this.Ptouch=0;
 
         this.rclk.style.width="20%";
         this.lclk.style.width="20%";
@@ -271,6 +295,16 @@ class mouseBC{
         this.lclk.style.float="left";
         this.rclk.style.borderRadius="3vmin";
         this.lclk.style.borderRadius="3vmin";
+        this.rclk.onmousedown=()=>{this.rDwn(0);};
+        this.lclk.onmousedown=()=>{this.lDwn(0);};
+        this.rclk.ontouchstart=()=>{this.rDwn(1);};
+        this.lclk.ontouchstart=()=>{this.lDwn(1);};
+        this.rclk.onmouseup=()=>{this.rUp(0);}
+        this.lclk.onmouseup=()=>{this.lUp(0);}
+        this.rclk.ontouchend=()=>{this.rUp(1);}
+        this.lclk.ontouchend=()=>{this.lUp(1);}
+        this.rclk.ondblclick=this.rdbclk;
+        this.lclk.ondblclick=this.ldbclk;
         this.scroll.type="range";
         this.scroll.min="0";
         this.scroll.max="100";
@@ -287,13 +321,181 @@ class mouseBC{
         content.appendChild(this.lclk);
     }
     scrollCh(val,m){}
+    rdbclk(){}
+    ldbclk(){}
     rDwn(){}
     rUp(){}
     lDwn(){}
-    lUp(){}
+    lUp(m){}
     mouseM(e,m){
         e.preventDefault();
-        console.log(m);}
+        if(!m){
+            console.log(e.movementX,e.movementY);
+        }
+        else{
+            let touch = e.touches[0];   
+            if (this.Ptouch) {
+                e.movementX = touch.pageX - this.Ptouch.pageX;
+                e.movementY = touch.pageY - this.Ptouch.pageY;
+            }
+            this.Ptouch = touch;
+        }
+    }
 }
-var c = new mouseBC();
+class keyMacro{
+    macro=["m1","m2","m3","m4","m5",'m6'];
+    td=[];
+    constructor(){
+        menuBpress(0);
+        this.div=document.createElement("div");
+        this.div.style.padding="5vmin";
+        this.getList();
+        for(let i=0;i<this.macro.length;i++){
+            this.td[i]=document.createElement("button");
+            this.td[i].onclick=()=>{this.macroB(i)};
+            this.td[i].style.width="15vmax";
+            this.td[i].style.padding="3vmax";
+            this.td[i].style.margin="1vmin";
+            this.td[i].style.borderRadius="2vmin";
+            this.td[i].style.fontSize="7vmin";
+            this.td[i].style.color="#960096";
+            this.td[i].style.backgroundColor=`rgb(${Math.floor(150+Math.random()*100)},${Math.floor(150+Math.random()*100)},255)`;
+            this.td[i].style.border='3vmin solid rgba(0,100,100,.2)';
+            this.td[i].innerHTML=this.macro[i];
+            this.div.appendChild(this.td[i]);
+        }
+        content.appendChild(this.div);
+    }
+    getList(){}
+    macroB(i){}
+}
+class duckyBC{
+    constructor(){
+        content.innerHTML="";
+        menuBpress(0);
+        this.textA=document.createElement("textarea");
+        this.textA.style.width="100%";
+        this.textA.style.height="80%";
+        this.textA.style.borderRadius="1vmax";
+        this.textA.style.backgroundColor="wheat";
+        this.textA.style.color="#7245e4";
+        this.textA.style.padding="1vw";
+        this.textA.style.fontSize="3vmin";
+        this.save=document.createElement("button");
+        this.run=document.createElement("button");
+        this.save.innerHTML="save";
+        this.run.innerHTML=">>"
+        this.save.style.fontSize="4vh";
+        this.run.style.fontSize="4vh";
+        this.save.style.padding="3vmin";
+        this.run.style.padding="3vmin";
+        this.save.style.float="left";
+        this.run.style.float="right";
+        this.run.style.color="red";
+        this.save.style.color="blue";
+        this.save.style.borderRadius="3vmin";
+        this.run.style.borderRadius="3vmin";
+        this.save.onclick=this.save;
+        this.run.onclick=this.run;
+
+        this.sel=document.createElement("select");
+        this.op1=document.createElement("option");
+        this.op2=document.createElement("option");
+        this.op3=document.createElement("option");
+        this.op1.innerHTML="S1";
+        this.op2.innerHTML="S2";
+        this.op3.innerHTML="S3";
+        this.op1.value="1";
+        this.op2.value="2";
+        this.op3.value="3";
+        this.sel.appendChild(this.op1);
+        this.sel.appendChild(this.op2);
+        this.sel.appendChild(this.op3);
+        this.sel.style.padding="3vmin";
+        this.sel.style.borderRadius="3vmin";
+        this.sel.style.float="right";
+        this.sel.style.fontSize="4vh";
+        this.sel.style.cursor="pointer";
+        
+        content.appendChild(this.textA);
+        content.appendChild(this.save);
+        content.appendChild(this.sel);
+        content.appendChild(this.run);
+    }
+    save(){}
+    run(){}
+}
+class keyMacroConfig{
+    td=[];
+    tde=[];
+    tr=[];
+    c1=[];
+    c2=[];
+    macro={};
+    constructor(){
+        content.innerHTML="";
+        menuBpress(0);
+        this.div=document.createElement("div");
+        this.table=document.createElement("table");
+        this.save=document.createElement("button");
+        this.add=document.createElement("button");
+        this.c1.push(this.save);
+        this.c2.push(this.add);
+        this.save.innerHTML="SAVE";
+        this.add.innerHTML="ADD";
+        this.td.push([this.c1[0],this.c2[0]]);
+        this.add.onclick=()=>{
+            this.c1.push(document.createElement("button"));
+            this.c2.push(document.createElement("button"));
+            this.c1[this.c1.length-1].innerHTML=`${this.c1.length-1}`;
+            this.c2[this.c2.length-1].innerHTML="add";
+            this.td.push([this.c1[this.c1.length-1],this.c2[this.c2.length-1]]);
+            this.refT();
+        };
+        this.refT();
+        this.div.style.width="80%";
+        this.div.style.margin="auto";
+        this.div.style.padding="5vmin";
+        this.table.style.width="100%";
+        this.div.appendChild(this.table);
+        content.appendChild(this.div);
+    }
+    refT(){
+        this.table.innerHTML="";
+        for (let i = 0; i < this.td.length; i++) {
+            this.td[i][0].style.width="100%";
+            this.td[i][1].style.width="100%";
+            this.td[i][0].style.height="100%";
+            this.td[i][1].style.height="100%";
+            this.td[i][0].style.fontSize="5vmin";
+            this.td[i][1].style.fontSize="5vmin";
+            this.td[i][0].style.color="rgb(150,0,150)";
+            this.td[i][1].style.color="rgb(150,100,100)";
+            this.td[i][0].setAttribute('id','c0m'+String(i));
+            this.td[i][1].setAttribute('id','c1m'+String(i));
+            if(i!=0){
+                this.td[i][1].onclick=()=>{this.macBPadd(i)};
+            }
+            this.tde[i]=[document.createElement('td'),document.createElement('td')];
+            this.tde[i][0].appendChild(this.td[i][0]);
+            this.tde[i][1].appendChild(this.td[i][1]);
+            this.tde[i][0].style.width="20%";
+            this.tde[i][1].style.width="80%";
+            this.tr[i]=document.createElement('tr');
+            this.tr[i].appendChild(this.tde[i][0]);
+            this.tr[i].appendChild(this.tde[i][1]);
+            this.table.appendChild(this.tr[i]);
+        }
+    }
+    macBPadd(i){
+            var macroname=prompt("macro_name(unique)");
+            this.macro[macroname]=prompt("macro sequence\nkeys seperated by '+'\ncheck about for key names");
+            document.getElementById('c'+1+'m'+i).innerHTML=this.macro[macroname];
+            document.getElementById('c'+0+'m'+i).innerHTML=macroname;
+    }
+}
+
+new pcBootC();
+new pcBootC();
+
 
